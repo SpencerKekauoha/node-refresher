4f7fc3be4e7a9c8707ad80e3435bccf2812b765e
SpencerKekauoha <spkekauoha@gmail.com>
2018-07-19 09:45:19 -0600

    setting up image imports

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -7,6 +22,17 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // Check if there is no new file to resize
+  if(!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  console.log(req.file);
 };
 
 // ES6 promise async await
@@ -23,7 +49,27 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // 1. Query the db for a list of all stores
   const stores = await Store.find();
-  console.log(stores);
-
   res.render('stores', { title: 'Stores', stores });
 }
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store givenn the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = 'Point';
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the updated store instead of the old one
+    runValidators: true,
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store.id}/edit`);
+  // redirect them to the store and tell them it worked
+};
